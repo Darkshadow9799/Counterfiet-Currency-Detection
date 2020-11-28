aaa4a337389f6daa632f977049328bfc49f4eef9
Darkshadow9799 <[jainaayush99.aj@gmail.com]>
2020-11-28 15:15:13 +0530

    AlexNet Network Gerneration

---
+++ AlexNet.py
@@ -0,0 +1,54 @@
+import tensorflow as tf
+
+model = tf.keras.models.Sequential(name="Alexnet")
+
+model.add(tf.keras.layers.Conv2D(filters=96,
+                                 kernel_size=(11,11),
+                                 strides=4,
+                                 input_shape=(224,224,3),
+                                 activation='relu',
+                                 padding="same",
+                                 name="Layer1"))
+model.add(tf.keras.layers.BatchNormalization(name="BatchNormalization_Layer1"))
+model.add(tf.keras.layers.MaxPool2D(pool_size=(3,3),
+                                    strides=(2,2),
+                                    name="MaxPool_Layer1"))
+
+model.add(tf.keras.layers.Conv2D(filters=256,
+                                 kernel_size=(5,5),
+                                 activation='relu',
+                                 padding="same",
+                                 name="Layer2"))
+model.add(tf.keras.layers.BatchNormalization(name="BatchNormalization_Layer2"))
+model.add(tf.keras.layers.MaxPool2D(pool_size=(3,3),
+                                    strides=(2,2),
+                                    name="MaxPool_Layer2"))
+
+model.add(tf.keras.layers.Conv2D(filters=384,
+                                 kernel_size=(3,3),
+                                 activation='relu',
+                                 padding="same",
+                                 name="Layer3"))
+
+model.add(tf.keras.layers.Conv2D(filters=256,
+                                 kernel_size=(3,3),
+                                 activation='relu',
+                                 padding="same",
+                                 name="Layer4"))
+model.add(tf.keras.layers.MaxPool2D(pool_size=(3,3),
+                                    strides=(2,2),
+                                    name="MaxPool_Layer4"))
+
+model.add(tf.keras.layers.Flatten(name="Flatten"))
+
+model.add(tf.keras.layers.Dense(units=4096, name="Dense1"))
+model.add(tf.keras.layers.Dropout(0.5, name="Dropout_Dense1"))
+
+model.add(tf.keras.layers.Dense(units=4096, name="Dense2"))
+model.add(tf.keras.layers.Dropout(0.5, name="Dropout_Dense2"))
+
+model.add(tf.keras.layers.Dense(units=2, name="Output"))
+
+model.add(tf.keras.layers.Activation('softmax'))
+
+print(model.summary())
