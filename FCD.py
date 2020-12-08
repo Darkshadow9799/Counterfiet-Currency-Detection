3ff88aa12a015c14d9ff5ac305c662a6a7b2abe0
Darkshadow9799 <[jainaayush99.aj@gmail.com]>
2020-12-08 11:16:53 +0530

    Checkpointing and CSVlogger

--- FCD.py
+++ FCD.py
@@ -1,4 +1,23 @@
 import AlexNet
+from keras.callbacks import ModelCheckpoint, CSVLogger
 
 model = AlexNet.built_model()
 model.summary()
+
+filepath = 'saved_models/weights-improvement-{epoch : 02d}-{val_acc : .2f}.h5'
+checkpoint = ModelCheckpoint(filepath=filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+
+log_csv = CSVLogger('my_logs.csv', separator=' , ', append=False)
+
+callable_list=[checkpoint, log_csv]
+
+'''
+model.fit_generator(
+ train_generator,
+ steps_per_epoch=2000 // batch_size,
+ epochs=500,
+ validation_data=validation_generator,
+ validation_steps=800 // batch_size,
+ callbacks=callback_list   
+)
+'''
