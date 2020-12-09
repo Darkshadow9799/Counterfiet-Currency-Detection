bd1915315225644c6bd612fe2bd4d90f9c02436b
Darkshadow9799 <[jainaayush99.aj@gmail.com]>
2020-12-09 10:46:50 +0530

    R500 Data Augmentation

--- Data_aug.py
+++ Data_aug.py
@@ -2,17 +2,21 @@ from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
 import os
 from tqdm import tqdm
 
- # add path here
-NEW_PATH = 'D:/AJ/Aayush/DE/Project/Data/Fake_500/' # saving dir
-PREFIX = 'f500' # prefix of image
+query = ["Fake_500", "Fake_2000", "Real_500","Real_2000"]
+
+NEW_PATH = 'D:/AJ/Aayush/DE/Project/Data/'+query[2]+'/'
+PREFIX = 'R500' # prefix of image
 
 datagen = ImageDataGenerator(
     rotation_range=20,
     zoom_range=0.2,
     brightness_range=[0.5,1.5]
 )
+
+query2 = ["F500","F2000","R500","R2000"]
+
 for i in tqdm(range(1, 22)):
-    PATH = 'D:/AJ/Aayush/DE/Project/Data/Resized/500/'+str(i)+'.jpg'
+    PATH = 'D:/AJ/Aayush/DE/Project/Data/Resized/'+query2[2]+'/'+str(i)+'.jpg'
     img = load_img(PATH)
     x = img_to_array(img)
     x = x.reshape((1, ) + x.shape)
--- Image_resizing_processing.py
+++ Image_resizing_processing.py
@@ -1,7 +1,10 @@
 from PIL import Image
 import os, sys
-path = 'D:/AJ/Aayush/DE/Project/Data/FakeCurrency/2000/'
-dirs = os.listdir( path )
+
+# ["FakeCurrency/500", "FakeCurrency/2000", "RealCurrency/500", "RealCurrency/2000"]
+
+path = 'D:/AJ/Aayush/DE/Project/Data/RealCurrency/500/'
+dirs = os.listdir(path)
 
 def resize():
     i=1
@@ -9,7 +12,7 @@ def resize():
         if os.path.isfile(path+item):
             im = Image.open(path+item)
             _, e = os.path.splitext(path+item)
-            f = 'D:/AJ/Aayush/DE/Project/Data/Resized/2000/'
+            f = 'D:/AJ/Aayush/DE/Project/Data/Resized/R500/'
             imResize = im.resize((507,224), Image.ANTIALIAS)
             imResize.save(f + str(i) + e, 'JPEG', quality=90)
             i+=1
